039e28491df982fcd3bc4aad7d059ef4f83c7617
Sharon Rumsey <sharon@rumsey.nz>
2016-02-27 17:51:25 +1300

    add youtube search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// define the component
+const SearchBar = () => {
+	return <input />;
+}
+
+// now to render to the html
+// index needs a reference for this file
+// exporting only searchbar component
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// api key for youtube
+const API_KEY = 'AIzaSyAPW0PBtc0rPGslcr-Ztdgv6nx-VeceIsg';
+
+import SearchBar from './components/search_bar';
 
 /****************************************
 react - js library used to produce html
@@ -10,10 +14,14 @@ nested -- returning jsx
 creating a new component that produces html
 then make sure it gets inside the DOM 
 *****************************************/
-// type of a component
+// type of a component - always one per file
 
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
