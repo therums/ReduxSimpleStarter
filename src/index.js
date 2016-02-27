c4d5f1be5e526550dc32b07767c80ac86fc0d98c
Sharon Rumsey <sharon@rumsey.nz>
2016-02-27 17:10:53 +1300

    got hi

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+/****************************************
+react - js library used to produce html
+individual components or views
+snippets of code that produce html
+multiple different components
+nested -- returning jsx
+creating a new component that produces html
+then make sure it gets inside the DOM 
+*****************************************/
+// type of a component
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+	return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
+
